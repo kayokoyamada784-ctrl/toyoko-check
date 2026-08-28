39d908fe04d334ae4ab8aaecdc55bc75bce5f198
kayokoyamada784-ctrl <kayokoyamada784@gmail.com>
2026-08-28 13:58:27 +0900

    Update check_toyoko.py

--- check_toyoko.py
+++ check_toyoko.py
@@ -10,4 +10,14 @@ response = requests.get(
 html = response.text
 
 for hotel in ["池袋", "大塚", "赤羽"]:
-    print(hotel, hotel in html)
+    pos = html.find(hotel)
+
+    if pos != -1:
+        start = max(0, pos - 300)
+        end = min(len(html), pos + 1000)
+
+        print("\n")
+        print("=" * 50)
+        print(hotel)
+        print("=" * 50)
+        print(html[start:end])
